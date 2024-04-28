8c95fc676d6a535ce6b63a80d349715ea2b56b3a
Rohit Talukdar <129711670+syncmee@users.noreply.github.com>
2024-04-28 12:11:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from art import logo
+
+print(logo)
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+if direction == "encode":
+
+    def encrypt(plain_text, shift_amount):
+        cipher_text = ""
+        for letter in plain_text:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        print(f"The encoded text is {cipher_text}")
+
+    encrypt(plain_text=text, shift_amount=shift)
+
+if direction == "decode":
+
+    def decrypt(plain_text, shift_amount):
+        cipher_text = ""
+        for letter in plain_text:
+            position = alphabet.index(letter)
+            new_position = position - shift_amount
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        print(f"The encoded text is {cipher_text}")
+
+    decrypt(plain_text=text, shift_amount=shift)
+
+should_end = False
+while not should_end:
+    restart = input(
+        "Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Thanks For Using Cipher")
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
